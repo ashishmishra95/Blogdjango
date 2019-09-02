070fc6b2d373b8c8e2717bd8fbe793bbf08c98cf
ashishmishra95 <=>
2019-09-03 01:31:43 +0530

    Post added via database and shell

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,19 @@
-from django.db import models
+# imported post model
+from django.db import models 
+from django.utils import timezone # for current dates on each post
+#imported user model
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+# We are creating models for post which contains title,content,descripion etc 
+class Post(models.Model):
+	title = models.CharField(max_length=100)
+	content = models.TextField()
+	date_posted = models.DateTimeField(default = timezone.now)
+	author = models.ForeignKey(User, on_delete= models.CASCADE) # on deletinf post will this be deleting user too
+
+
+# To make our post details descriptive for shell we can use
+	def __str__(self):
+		return self.title
--- blog/views.py
+++ blog/views.py
@@ -1,25 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
 #We are creating dummy data that can be accessed through our template which we have rendered
 
-posts = [
-	{
-			'author': 'Ashish Mishra',
-			'date_posted':'16-08-2019',
-			'title':'Blog post 1',
-			'content':'This is gonna be viral soon!',
+# posts = [
+# 	{
+# 			'author': 'Ashish Mishra',
+# 			'date_posted':'16-08-2019',
+# 			'title':'Blog post 1',
+# 			'content':'This is gonna be viral soon!',
 
-	},
-	{	
-			'author':'Ghanshyam Gupta',
-			'date_posted':'16-08-2019',
-			'title':'Blog post 2',
-			'content':'Yeah actually',
+# 	},
+# 	{	
+# 			'author':'Ghanshyam Gupta',
+# 			'date_posted':'16-08-2019',
+# 			'title':'Blog post 2',
+# 			'content':'Yeah actually',
 
-	}
+# 	}
 
-]
+# ]
 
 # abouts = [
 
@@ -41,7 +42,7 @@ posts = [
 def home(request):
 	# Whatever we have defined we are accessing from here
 	context = {
-	'posts': posts, # key (posts)
+	'posts':  Post.objects.all(),#posts, # key (posts)
 	}
 	# Loading of template in another way using render function which take 2 arguments i.e request and template which we want to fetch
 	return render(request, 'blog/home.html',context) # we used sub folder name which we have created under templated folder i.e blog
